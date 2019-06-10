c9bd497384ed71ec0342749ab690bf4509e68732
vhqd <www.zhangqiangwo@qq.com>
2019-06-10 18:45:26 +0800

    addmepage

--- src/components/Me.vue
+++ src/components/Me.vue
@@ -1,15 +1,17 @@
 <template>
-    <div>
-        <van-nav-bar title="关于我们"/>
-    </div>
+  <div>
+    <van-nav-bar title="详细"/>
+    <van-cell-group>
+      <van-cell title="关于我们" value=">"/>
+      <van-cell title="联系我们" value=">"/>
+    </van-cell-group>
+  </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-
-        }
-    }
-}
+  data() {
+    return {};
+  }
+};
 </script>
